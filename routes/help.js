e2a596dbcb2b883fcc239a132a04bcd453e1b3ce
Jesse Akorah <jesseakorah@gmail.com>
2024-02-01 16:13:15 +0100

    Add admin authorization check to post help routes

--- routes/help.js
+++ routes/help.js
@@ -5,15 +5,21 @@ const Help = require("../controllers/help");
 
 const auth = require("../middleware/index");
 
-router.post("/topics", auth.authToken, Help.createTopic);
+router.post("/topics", auth.authToken, auth.isAdmin, Help.createTopic);
 router.get("/topics", Help.getTopics);
-router.post("/topics/documents", auth.authToken, Help.createDoc);
+router.post("/topics/documents", auth.authToken, auth.isAdmin, Help.createDoc);
 router.get("/topics/documents", Help.getDocs);
-router.post("/question-hub", auth.authToken, Help.createQuestionTopic);
+router.post(
+  "/question-hub",
+  auth.authToken,
+  auth.isAdmin,
+  Help.createQuestionTopic
+);
 router.get("/question-hub", Help.getQuestionTopics);
 router.post(
   "/question-hub/question/:topicId",
   auth.authToken,
+  auth.isAdmin,
   Help.createHelpOption
 );
 router.get("/question-hub/question", Help.getHelpOptions);
