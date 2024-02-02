0db80f92e4fa613d705b5df13887d0373c7beac5
Jesse Akorah <jesseakorah@gmail.com>
2024-02-02 09:33:25 +0100

    Update for aws

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const { Server } = require("socket.io");
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// const server = app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
 
 const io = new Server(server, {
   pingTimeout: 60000,
