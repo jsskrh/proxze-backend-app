8efec3f12bbe583631a047f7a892104775ac24ea
toluOjo <ojotolulope137@gmail.com>
2024-09-24 09:01:13 +0100

    fix location label

--- utils/tasks.js
+++ utils/tasks.js
@@ -36,7 +36,7 @@ const taskCreator = async ({
     // lga,
     // address,
     location: {
-      label: location.label || "proxzeBusiness",
+      label: location.label || address,
       geometry: {
         type: "Point",
         coordinates: [location.coords.lng, location.coords.lat],
@@ -60,7 +60,7 @@ const taskCreator = async ({
     group,
     isProxzeBusiness,
     request,
-    address
+    address,
   });
 
   // const usersWithinRadius = await User.find({
